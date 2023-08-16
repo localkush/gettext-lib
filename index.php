eeb04c19fbdf6d32bd50b187b4e3f654d3ef3921
localkush <78888125+localkush@users.noreply.github.com>
2023-08-16 15:07:55 -0400

    Update and rename lib/index.php to index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+```php
+require_once 'lib/gettext.php';
+require_once 'lib/streams.php';
+
+$language = 'en_US';
+
+$locale_lang = $language;
+$locale_file = new FileReader("languages/$locale_lang/LC_MESSAGES/messages.mo");
+$locale_fetch = new gettext_reader($locale_file);
+
+function _e($text){
+    global $locale_fetch;
+    return $locale_fetch->translate($text);
+}
+
+echo _e('Hello World'); // this will be translated.
+```
